cb34e99d507f417110cded89b251e0825963a2e4
Himanshu-Bohra <102330942+Himanshu-Bohra@users.noreply.github.com>
2024-02-11 23:02:41 -0500

    Create Assignment1SpringNasaApodApplication.java
    main application

---
+++ Assignment1SpringNasaApodApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment1springnasaapod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1SpringNasaApodApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Assignment1SpringNasaApodApplication.class, args);
+	}
+}
